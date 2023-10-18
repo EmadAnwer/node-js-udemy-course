c9db31c822735c6d75e93834351896e3bc79f8c4
Emad Anwer <emadanwer.official@gmail.com>
2023-10-18 14:25:14 +0300

    handle dynamic content using pug

--- exress-section-6/routes/shop.js
+++ exress-section-6/routes/shop.js
@@ -2,12 +2,20 @@ const path = require('path');
 
 const express = require('express');
 
+const adminData = require('./admin');
 const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // console.log('products form shop', adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  console.log('products form shop', adminData.products);
+  res.render('shop', {
+    prods: adminData.products,
+    pageTitle: 'Shop',
+    path: '/',
+  });
 });
 
 module.exports = router;
