2d8b559b7df1861dd401f366d8b863741e861007
Emad Anwer <emadanwer.official@gmail.com>
2023-10-19 10:20:43 +0300

    ejs support

--- exress-section-6/app.js
+++ exress-section-6/app.js
@@ -2,20 +2,11 @@ const path = require('path');
 
 const express = require('express');
 
-const expressHandlebars = require('express-handlebars');
 const app = express();
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-app.engine(
-  'hbs',
-  expressHandlebars({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs',
-  })
-);
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true }));
@@ -25,7 +16,7 @@ app.use('/admin', adminData.router);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: '404' });
+  res.status(404).render('404', { pageTitle: '404', path: '' });
 });
 
 app.listen(3000);
